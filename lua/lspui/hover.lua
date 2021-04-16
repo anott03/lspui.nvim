5401c902793690bf454cd05db4d0923e884b7917
anott03 <amitav_nott@ryecountryday.org>
2021-04-16 14:46:33 -0400

    hover doc stuff

--- lua/lspui/hover.lua
+++ lua/lspui/hover.lua
@@ -6,7 +6,7 @@ local M = {}
 
 M.hover_doc = function()
   local params = util.make_position_params()
-  lsp.buf_request(0,'textDocument/hover', params, function(_, method, result)
+  lsp.buf_request(0,'textDocument/hover', params, function(_, _, result)
     if not (result and result.contents) then
       return
     end
@@ -18,11 +18,31 @@ M.hover_doc = function()
       return
     end
 
+    local ft
+    local _width = 40
+    for i, k in pairs(markdown_lines) do
+      if k:sub(1,3) == '```' then
+        if #k > 3 then
+          ft = k:sub(4, -1)
+        end
+        table.remove(markdown_lines, i)
+      end
+    end
+
+    for _, k in pairs(markdown_lines) do
+      if #k > _width then _width = #k end
+    end
+
     win.create_win({
       text = markdown_lines,
       height = #markdown_lines,
-      set_buf_settings = function(_, win_id)
+      width = _width,
+      focusable = false,
+      set_buf_settings = function(bufh, win_id)
         api.nvim_win_set_option(win_id, 'winhl', 'Normal:LspuiNormal')
+        api.nvim_buf_set_keymap(bufh, "n", "<esc>", "<cmd>q!<cr>", {})
+        api.nvim_buf_set_option(bufh, "modifiable", false)
+        api.nvim_buf_set_option(bufh, "filetype", ft)
       end
     })
   end)
